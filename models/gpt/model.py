4539c22672b698eaad2038e9facc801ba1a616e8
ibusnowden <iniang1@southwest.tn.edu>
2025-08-20 14:30:57 -0500

    added a custom GPT model -> decoder only model

--- models/gpt/model.py
+++ models/gpt/model.py
@@ -1,1 +1,24 @@
 # GPT-specific model definition (decoder-only)
+import torch
+import os, sys
+sys.path.append('..')
+import torch.nn as nn
+from models.base import TransformerBlock
+from layers.embeddings import TokenEmbedding, PositionalEncoding
+
+
+
+class GPT(nn.Module):
+    def __init__(self, vocab_size, d_model, d_ff, num_heads, num_layers, max_len=512):
+        super().__init__()
+        self.token_embedding = TokenEmbedding(vocab_size, d_model)
+        self.pos_embedding = PositionalEncoding(d_model, max_len)
+        self.layers = nn.ModuleList([TransformerBlock(d_model, num_heads, d_ff)] for _ in range(num_layers))
+        self.to_logits = nn.Linear(d_model, vocab_size)
+
+    def forward(self, x):
+        x = self.token_embedding(x)
+        x = self.pos_embedding(x)
+        for layer in self.layers:
+            x = layer(x)
+        return self.to_logits(x)
