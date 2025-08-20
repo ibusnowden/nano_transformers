f088b952408aba740e624b3a2f5dfa3afd2ebfa0
ibusnowden <iniang1@southwest.tn.edu>
2025-08-20 12:46:23 -0500

    updating the embedding dimension and the postional encoding

--- layers/embeddings.py
+++ layers/embeddings.py
@@ -1,1 +1,24 @@
 # Contains Token and Positional Embeddings
+import torch.nn as nn
+import torch
+import math
+
+class TokenEmbedding(nn.Module):
+    def __init__(self, vocab_size, d_model):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x):
+        return self.embedding(x)
+    
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_len=512):
+        super().__init__()
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div) # even position
+        pe[:, 1::2] = torch.cos(position * div) # odd position
+
+    def forward(self, x):
+        return x + self.pe[:, :x.size(1), :]
