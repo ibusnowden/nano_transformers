920c3eba5869e1468a6521bd7e98a2d761b688d1
ibusnowden <iniang1@southwest.tn.edu>
2025-08-20 12:32:34 -0500

    updated the feed-forward layer with relu

--- layers/feed_forward.py
+++ layers/feed_forward.py
@@ -1,1 +1,14 @@
 # Contains the simple FFN block
+import torch.nn as nn
+
+class FeedForward(nn.Module):
+    def __init__(self, d_model, d_ff):
+        super().__init__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.w_2 = nn.Linear(d_ff, d_model)
+        self.ffn = nn.ReLU()
+
+    def forward(self, x):
+        return self.w_2(self.ffn(self.w_1(x)))
+    
+# considr adding ffn with silu for other moe model like qween/mistral
