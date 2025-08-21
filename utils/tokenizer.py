ccbd2babef9904c0d4291946995b279903d1459b
ibusnowden <iniang1@southwest.tn.edu>
2025-08-21 13:47:24 -0500

    applying a simple world level tokenizer

--- utils/tokenizer.py
+++ utils/tokenizer.py
@@ -1,1 +1,12 @@
 # A simple tokenizer
+class SimpleTokenizer:
+    def __init__(self, vocab):
+        self.vocab = vocab
+        self.stoi = {s:i for i, s in enumerate(vocab)}
+        self.itos = {i:s for s, i in enumerate(vocab)}
+
+    def encode(self, text):
+        return [self.stoi[c] for c in text]
+    
+    def decode(self, tokens):
+        return "".join([self.itos[i] for i in tokens])
